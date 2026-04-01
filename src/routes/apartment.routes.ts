2a2db264bf80f49b4a6cb0eef3a928c051fc0dcc
ssk7594-CRz <ssk7594@gmail.com>
2026-04-01 15:44:06 +0900

    fix: conflict 해결

--- src/routes/apartment.routes.ts
+++ src/routes/apartment.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import * as apartmentController from "../controllers/apartment.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-// 공개용 (인증 불필요)
+// 공개??(?�증 불필??
 router.get("/public", apartmentController.getApartmentsPublic);
 router.get("/public/:id", apartmentController.getApartmentPublicById);
 
-// 관리자 전용 (인증 필요)
-// TODO: 권한 체크 미들웨어 추가 필요
+// 관리자 ?�용 (?�증 ?�요)
+// TODO: 권한 체크 미들?�어 추�? ?�요
 router.get("/", authMiddleware, apartmentController.getApartments);
 router.get("/:id", authMiddleware, apartmentController.getApartmentById);
 
